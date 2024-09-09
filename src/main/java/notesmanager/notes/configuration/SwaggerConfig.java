8086f007be761608036fca9a7c8079ac1d1f9ef6
Vasquez <antoniovasquez1216@gmail.com>
2024-09-09 16:02:02 -0500

    swagger agregado

--- src/main/java/notesmanager/notes/configuration/SwaggerConfig.java
+++ src/main/java/notesmanager/notes/configuration/SwaggerConfig.java
@@ -1,24 +1,20 @@
 package notesmanager.notes.configuration;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                                                    .select()
-                                                    .apis(RequestHandlerSelectors
-                                                    .basePackage("snotesmanager.notes.controllers"))
-                                                    .paths(PathSelectors.any()).build();
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/**") // Puedes ajustar el patrón de las rutas que quieres incluir
+                .packagesToScan("notesmanager.notes.controllers") // Ajusta el paquete según tu estructura
+                .build();
     }
-    
 }
